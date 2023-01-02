ea77a45de0237b747dc803333d86cdc5dfe23d5b
编程菜狗 <7409165+programming_dog@user.noreply.gitee.com>
2023-01-02 22:00:46 +0800

    use seperate files to place business logics

--- BSV/BSVDlg.cpp
+++ BSV/BSVDlg.cpp
@@ -875,46 +875,6 @@ void CBSVDlg::OnBnClickedSavepic2()
 	Savepic(m_RightCamera, m_RightImage, true);
 }
 
-void Image::Initial()
-{
-	InitialBmpInfo();
-	OriDIBBits = NULL;
-	DIBBits = NULL;
-	ShowDIBBits = NULL;
-	Width = 0;
-	Height = 0;
-	RectLeft = 0;
-	RectBottom = 0;
-	Bit = 8;
-	OriWidth = 1600;
-	OriHeight = 1200;
-	BufHandle = false;
-}
-
-void Image::InitialBmpInfo()
-{
-	BmpInfo = (LPBITMAPINFO) new
-		char[sizeof(BITMAPINFOHEADER) + sizeof(RGBQUAD) * 256];//设置图像信息头
-	BmpInfo->bmiHeader.biBitCount = 8;
-	BmpInfo->bmiHeader.biClrImportant = 0;
-	BmpInfo->bmiHeader.biClrUsed = 0;
-	BmpInfo->bmiHeader.biCompression = BI_RGB;
-	BmpInfo->bmiHeader.biPlanes = 1;
-	BmpInfo->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-	BmpInfo->bmiHeader.biXPelsPerMeter = 0;
-	BmpInfo->bmiHeader.biYPelsPerMeter = 0;
-	BmpInfo->bmiHeader.biSizeImage = 0;
-	BmpInfo->bmiHeader.biHeight = OriHeight;
-	BmpInfo->bmiHeader.biWidth = OriWidth;
-	for (int i = 0; i < 256; i++)
-	{
-		BmpInfo->bmiColors[i].rgbBlue = i;
-		BmpInfo->bmiColors[i].rgbGreen = i;
-		BmpInfo->bmiColors[i].rgbRed = i;
-		BmpInfo->bmiColors[i].rgbReserved = 0;
-	}
-}
-
 void Calibration::Initial()
 {
 	RowCornerNum = 0;
--- BSV/BSVDlg.h
+++ BSV/BSVDlg.h
@@ -8,6 +8,7 @@
 #include "MVImage.h"
 #include "FeatureDetect.h"
 #include "Camera.h"
+#include "Image.h"
 #include "mscomm.h"
 #include "opencv2/opencv.hpp"
 #include "opencv2/core/core.hpp"
@@ -27,26 +28,6 @@ const string RightCameraCalibrationResultText = "RCamera_caliberation_result.txt
 const UINT_PTR FirstTimerId = 1;
 const UINT_PTR SecondTimerId = 2;
 const UINT CircleDetectTimerElapse = 300;
-
-class Image
-{
-public:
-	void Initial();
-	void InitialBmpInfo();
-public:
-	BITMAPINFO* BmpInfo;//图像信息头指针
-	LPSTR OriDIBBits;//原始图像，从图像文件读入或相机中直接获取。灰度图像，如果是RGB直接转化为8位灰度图
-	LPSTR DIBBits;//要进行处理的图像，有可能是整幅图像也有可能是原始图像的一个区域
-	LPSTR ShowDIBBits;//要进行显示的图像
-	LONG Width;//要进行处理的图像的宽度，如果对图像区域进行处理，该宽度与原始图像的宽度是不同的
-	LONG Height;//要进行处理的图像的高度，如果对图像区域进行处理，该宽度与原始图像的宽度是不同的
-	LONG RectLeft;//图像选择区域的左边界
-	LONG RectBottom;//图像选择区域的下边界
-	int Bit;//图像位数
-	int OriWidth;//图像原始宽度
-	int OriHeight;//图像原始高度
-	BOOL BufHandle;//图像可以进行处理标识,当每次调用左相机回调函数是,该标记置true,当对图像进行处理时,该标记置false
-};
 
 //双目相机参数
 class Calibration
